ebb857257e38b43afa858939096945c9bed13a3c
seanraad123 <seanconrad123@gmail.com>
2018-07-31 09:49:00 -0400

    added the funcationality of posting a photo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
 
-
   def index
     @posts = Post.all
     @users = User.all
@@ -18,7 +17,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(set_params)
-  
+
     if @post.save
       flash[:notice] = "Account created!"
       redirect_to posts_path
@@ -31,9 +30,5 @@ class PostsController < ApplicationController
   def set_params
     params.require(:post).permit(:caption, :picture, :user_id)
   end
-
-
-
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_184200) do
+ActiveRecord::Schema.define(version: 2018_07_31_132119) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 2018_07_30_184200) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_file_name"
+    t.string "picture_content_type"
+    t.integer "picture_file_size"
+    t.datetime "picture_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
