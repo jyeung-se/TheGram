4c37628f663291cc5926cea5b01e62c71719b514
seanraad123 <seanconrad123@gmail.com>
2018-08-01 19:01:29 -0400

    Added Like button functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -15,31 +15,13 @@ class LikesController < ApplicationController
 
     if @post.likes.length == 0
       Like.create(like_count: 0, post_id: params[:post], user_id: current_user.id)
+      @post.likes << Like.last
     end
 
     like = @post.likes.first
     like.like_count += 1
     like.save
 
-
-    # if @post.likes.any?
-    #   if current_user.likes.any?
-    #     current_user.likes.each do |like|
-    #
-    #       if like.post_id == @post.id # if true, decrament like_count
-    #         if @post.likes.first.like_count > 0
-    #           @post.likes.first.like_count -= 1
-    #           @post.save
-    #         end
-    #       else
-    #         @post.likes.first.like_count += 1
-    #         @post.save
-    #       end
-    #     end
-    #   end
-    # else
-    #   @like = Like.create(like_count: 0, post_id: params[:post], user_id: current_user.id)
-    # end
     redirect_to posts_path
   end
 
