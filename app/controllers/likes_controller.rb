fe469bc5cca7a841e2a76d0ffeed04f23eace1f6
seanraad123 <seanconrad123@gmail.com>
2018-08-01 18:18:14 -0400

    Added likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,26 +11,36 @@ class LikesController < ApplicationController
   end
 
   def create
-    @post = Post.find(params[:id])
-    if @post.like.any?
+    @post = Post.find(params[:post])
+
+    if @post.likes.length == 0
+      Like.create(like_count: 0, post_id: params[:post], user_id: current_user.id)
+    end
+
+    like = @post.likes.first
+    like.like_count += 1
+    like.save
 
 
-
-    else
-      @like = Like.create(like_count: 0, post_id: params[:post], user_id: current_user.id)
-
+    # if @post.likes.any?
+    #   if current_user.likes.any?
+    #     current_user.likes.each do |like|
+    #
+    #       if like.post_id == @post.id # if true, decrament like_count
+    #         if @post.likes.first.like_count > 0
+    #           @post.likes.first.like_count -= 1
+    #           @post.save
+    #         end
+    #       else
+    #         @post.likes.first.like_count += 1
+    #         @post.save
+    #       end
+    #     end
+    #   end
+    # else
+    #   @like = Like.create(like_count: 0, post_id: params[:post], user_id: current_user.id)
+    # end
     redirect_to posts_path
-      # current_user.likes.each do |like| # Get all of the likes from a person
-      #   if like.post_id != @post.id #get this variable
-      #     post = Post.find(like.post_id)
-      #     like = post.likes.first
-      #     like.like_count += 1
-      #     like.save
-      #   end
-      #     @post.likes.first.like_count -= 1
-      #     #find post_id somehow and update the like count
-      #   end
-      #
-      # end
   end
+
 end
