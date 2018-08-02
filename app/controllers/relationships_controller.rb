49f0a4a2a6b98fc40426f23f8384a09a1077eb52
Jack Yeung <jyeung.se@gmail.com>
2018-08-02 14:23:56 -0400

    Completed Fully Functional 'Follow' & 'Unfollow' buttons

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,19 +3,19 @@ class RelationshipsController < ApplicationController
   before_action :authorized
 
   def create
-    @user = User.find(params[:relationship][:followed_id])
-    current_user.follow!(@user)
+    person_to_follow = User.find(params[:relationship][:followed_id])
+    current_user.following << person_to_follow
     flash[:notice] = "You are now following."
-    redirect_to @user
+    redirect_to user_path(current_user)
   end
 
   def destroy
     # Unfollows other user.
     # Destroys relationship between two users
     # @user should be the other user and not yourself
-    
-    @user = Relationship.find(params[:id]).followed
-    current_user.unfollow!(@user)
+
+    person_to_unfollow = Relationship.find(params[:id]).followed
+    current_user.unfollow!(person_to_unfollow)
 
     flash[:notice] = "Unfollow completed."
     redirect_to @user
