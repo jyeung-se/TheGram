e168805d4fb502657b2d81f4592d24064b691b68
seanraad123 <seanconrad123@gmail.com>
2018-08-02 10:16:41 -0400

    working on likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -13,16 +13,31 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post])
 
-    if @post.likes.length == 0
-      Like.create(like_count: 0, post_id: params[:post], user_id: current_user.id)
-      @post.likes << Like.last
+    if params[:like?] == "like"
+      if @post.likes.length == 0
+        Like.create(like_count: 0, post_id: params[:post], user_id: current_user.id)
+        @post.likes << Like.last
+      end
+      like = @post.likes.first
+      like.like_count += 1
+      like.save
+    elsif params[:like?] == "unlike"
+      like = @post.likes.first
+      if like.like_count > 0
+        like.like_count -= 1
+        like.save
+
+        # Find my association with that post and delete it
+        
+        # delete_me = current_user.likes.find_by(post_id: @post.id)
+        # delete_me.destroy
+      end
     end
-
-    like = @post.likes.first
-    like.like_count += 1
-    like.save
-
     redirect_to posts_path
+  end
+
+  def like
+    #code
   end
 
 end
